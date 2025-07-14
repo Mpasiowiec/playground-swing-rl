4463e690bae492e6f40f1f31f8156fa852fe080f
Mpasiowiec <102516644+Mpasiowiec@users.noreply.github.com>
2025-07-14 19:36:55 +0200

    torque <> accel

--- env/playground_swing.py
+++ env/playground_swing.py
@@ -39,13 +39,17 @@ class PlaygroundSwingEnv(gym.Env):
         self.b = (self.m1 + self.m2/2)*self.l2/self.M
 
         # range of torso position
-        self.phi_mean = np.radians(10)
-        self.phi0 = np.radians(30)
-        # range of legs position
-        self.psi_mean = np.radians(10)
-        self.psi0 = np.radians(45)
-        # body torque (form perplexity)
-        self.max_body_torque = 150
+        # self.phi_mean = np.radians(10)
+        # self.phi0 = np.radians(30)
+        self.max_phi = np.radians(40)
+        self.min_phi = np.radians(-20)
+        # # range of legs position
+        # self.psi_mean = np.radians(10)
+        # self.psi0 = np.radians(45)
+        self.max_psi = np.radians(55)
+        self.min_psi = np.radians(-35)
+        # body angular acceleration
+        self.max_body_accel = 8
 
         # Moments of inertia
         self.I = (self.M0/3 + self.M) * self.L**2
@@ -58,12 +62,12 @@ class PlaygroundSwingEnv(gym.Env):
         self.N = (self.M0/2 + self.M)*self.L
 
         # position of swing theta, speed of swing theta_dot (limit full rotation speed of math pendulum), position of torso phi, position of legs psi
-        high = np.array([pi, sqrt(5*self.L*self.g), self.phi_mean+self.phi0, 25, self.psi_mean+self.psi0, 25], dtype=np.float32)
-        low = np.array([-pi, -sqrt(5*self.L*self.g), self.phi_mean-self.phi0, -25, self.psi_mean-self.psi0, -25], dtype=np.float32)
+        high = np.array([pi, sqrt(5*self.L*self.g), self.max_phi,10, self.max_psi,10], dtype=np.float32)
+        low = np.array([-pi, -sqrt(5*self.L*self.g), self.min_phi,-10, self.min_psi,-10], dtype=np.float32)
         self.observation_space = spaces.Box(low=low, high=high, dtype=np.float32)
         
         self.action_space = spaces.Box(
-            low=-self.max_body_torque, high=self.max_body_torque, shape=(2,), dtype=np.float32
+            low=-self.max_body_accel, high=self.max_body_accel, shape=(2,), dtype=np.float32
         )
 
     def step(self, u):
@@ -102,7 +106,7 @@ class PlaygroundSwingEnv(gym.Env):
                     -k*np.sign(theta_dot)*(theta_dot**2*L**3 + k_prime)
             return torque/((I+I_prime) + m1*l1*(-L*cos(phi) + a*sin(phi)) + m3*l3*(L*cos(psi) + b*sin(psi)))
         
-        u = np.clip(u, -self.max_body_torque, self.max_body_torque)
+        u = np.clip(u, -self.max_body_accel, self.max_body_accel)
         
         # the higher speed the better 
         reward = theta_dot
@@ -110,12 +114,31 @@ class PlaygroundSwingEnv(gym.Env):
         phi_ddot = u[0]
         psi_ddot = u[1]
         
+        # # if torque would get body position out of bound then its zero
+        if phi_ddot > 0 and phi == self.max_phi:
+            phi_ddot=0
+        if phi_ddot < 0 and phi == self.min_phi:
+            phi_ddot=0
+        if psi_ddot > 0 and psi == self.max_psi:
+            psi_ddot=0
+        if psi_ddot < 0 and psi == self.min_psi:
+            psi_ddot=0
+                
         phi_dot += phi_ddot*dt
         psi_dot += psi_ddot*dt
+               
+        if phi_dot > 0 and phi == self.max_phi:
+            phi_dot=0
+        if phi_dot < 0 and phi == self.min_phi:
+            phi_dot=0
+        if psi_dot > 0 and psi == self.max_psi:
+            psi_dot=0
+        if psi_dot < 0 and psi == self.min_psi:
+            psi_dot=0
         
-        phi += phi_dot*dt + (phi_ddot*dt**2)/2
-        psi += psi_dot*dt + (psi_ddot*dt**2)/2
-
+        phi = np.clip(phi + phi_dot*dt + (phi_ddot*dt**2)/2, self.min_phi, self.max_phi)
+        psi = np.clip(psi + psi_dot*dt + (psi_ddot*dt**2)/2, self.min_psi, self.max_psi)
+        
         theta, theta_dot = RK4_for_2nd_order_ODE(eq_theta_ddot, dt, dt, theta, theta_dot)
 
         self.state = np.array([theta, theta_dot, phi, phi_dot, psi, psi_dot])
@@ -129,16 +152,16 @@ class PlaygroundSwingEnv(gym.Env):
         if options is None:
             theta = np.radians(33)
             theta_dot = 0
-            phi = self.phi_mean+self.phi0
+            phi = self.max_phi
             phi_dot = 0
-            psi = self.psi_mean+self.psi0
+            psi = self.max_psi
             psi_dot = 0
         else:            
             theta = options.get("theta_init") if "theta_init" in options else np.radians(33)
             theta_dot = options.get("theta_dot_init") if "theta_dot_init" in options else 0
-            phi = options.get("phi_init") if "phi_init" in options else self.phi_mean+self.phi0
+            phi = options.get("phi_init") if "phi_init" in options else self.max_phi
             phi_dot = options.get("phi_dot_init") if "phi_dot_init" in options else 0
-            psi = options.get("psi_init") if "psi_init" in options else self.psi_mean+self.psi0
+            psi = options.get("psi_init") if "psi_init" in options else self.max_psi
             psi_dot = options.get("psi_dot_init") if "psi_dot_init" in options else 0
             
             theta = utils.verify_number_and_cast(theta)
@@ -149,7 +172,7 @@ class PlaygroundSwingEnv(gym.Env):
             psi_dot = utils.verify_number_and_cast(psi_dot)
             
         high = np.array([theta, theta_dot, phi, phi_dot, psi, psi_dot])
-        low = np.array([-theta, -theta_dot, phi-2*self.phi0, -phi_dot, psi-2*self.psi0, -psi_dot])
+        low = np.array([-theta, -theta_dot, self.min_phi, -phi_dot, self.min_psi, -psi_dot])
         self.state = self.np_random.uniform(low=low, high=high)
         self.last_u = None
 
